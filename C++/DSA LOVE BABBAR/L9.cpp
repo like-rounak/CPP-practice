4d81e3dec2567f757c8159429b40e14cb7e6fb00
like-rounak <rounaks.it.ug@jadavpuruniversity.in>
2023-08-18 13:34:25 +0530

    update

--- C++/DSA LOVE BABBAR/L9.cpp
+++ C++/DSA LOVE BABBAR/L9.cpp
@@ -4,8 +4,15 @@
 using namespace std;
 
 //HOLD
-void printArray(){
-
+void printArray(int arr[], int size){
+    cout << "Printing the array ..." << endl;
+    //printimg array loop
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
 }
 
 int main(){
@@ -13,16 +20,24 @@ int main(){
     int arr[15];
 
     //access
-    cout << arr[2]<<endl;
+    cout << arr[2] <<endl;
 
     //Initialise
     int arr2[3]={4,5,11};
+    int zero[10]={0}; //all zero
 
-    //access arr2
+    //access arr2 
 
     cout << "Element at 2 index = " << arr2[2] << endl;
     
-    
+    //printing array
+    printArray(zero,10);
+    printArray(arr2,3);
+
+    //size 
+    //locations - sizeof/int 
+    cout<< " No. of Locations in arr[15] : " << sizeof(arr)/sizeof(int) << endl;
+
     
     return 0;
 }
