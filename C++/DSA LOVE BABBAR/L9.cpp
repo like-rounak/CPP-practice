9c64f8a1fdd010dbf1ca2bacd7eca15526b8ddb7
like-rounak <rounaks.it.ug@jadavpuruniversity.in>
2023-08-18 12:57:09 +0530

    l9 - array

---
+++ C++/DSA LOVE BABBAR/L9.cpp
@@ -0,0 +1,28 @@
+//Arrays 
+
+#include<iostream>
+using namespace std;
+
+//HOLD
+void printArray(){
+
+}
+
+int main(){
+    //declare
+    int arr[15];
+
+    //access
+    cout << arr[2]<<endl;
+
+    //Initialise
+    int arr2[3]={4,5,11};
+
+    //access arr2
+
+    cout << "Element at 2 index = " << arr2[2] << endl;
+    
+    
+    
+    return 0;
+}
