6beb05e694441cecdcc2fdc7e23934d4b242f607
like-rounak <rounaks.it.ug@jadavpuruniversity.in>
2023-10-10 19:41:34 +0530

    update , rough

--- C++/CODE WITH HARRY/1.cpp
+++ C++/CODE WITH HARRY/1.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
-using namespace std;
-enum Day {Sunday,Monday,Tuesday,Wednesday};
-int main() {
-    enum day;
-    cin>>day;
-    cout<<day;
+// check prime 
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int flag=0;
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0){
+        cout<<"Prime";
+    }
+    else{
+        cout<<"Not Prime";
+    }
     return 0;
 }
